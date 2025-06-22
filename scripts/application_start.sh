b1d84716b93703471daaf572f7670bb02f9a1791
SaajidPasha10 <89309945+SaajidPasha10@users.noreply.github.com>
2025-06-23 02:56:20 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'start app' >> /home/ec2-user/fastapi-codedeploy/deploy.log
-cd /home/ec2-user/fastapi-codedeploy
+echo 'start app' >> /home/ec2-user/Fastapi-Code-pipeline/deploy.log
+cd /home/ec2-user/Fastapi-Code-pipeline
 source venv/bin/activate
 nohup uvicorn app:app --host 0.0.0.0 --port 8000 >> deploy.log 2>&1 &
