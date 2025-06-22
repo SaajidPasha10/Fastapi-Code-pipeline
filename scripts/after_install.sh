fe017ebdce182adc7e7d3e3d4d215e33ccdf631d
SaajidPasha10 <89309945+SaajidPasha10@users.noreply.github.com>
2025-06-23 02:55:47 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'after_install' >> /home/ec2-user/fastapi-codedeploy/deploy.log
+echo 'after_install' >> /home/ec2-user/Fastapi-Code-pipeline/deploy.log
 cd /home/ec2-user/fastapi-codedeploy
 python3 -m venv venv || true
 source venv/bin/activate
