79106a99d8676dc7d28f3e24e68abba8c58d39fb
SaajidPasha10 <89309945+SaajidPasha10@users.noreply.github.com>
2025-06-23 06:11:09 +0530

    Update post_deploy.sh

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd /home/ec2-user/Fastapi-Code-pipeline/Fastapi-Code-pipeline
+# Update code
 git fetch --all
 git reset --hard origin/main
 
-pm2 delete fastapi-app || true
-pm2 start venv/bin/uvicorn app:app --name fastapi-app --interpreter bash -- --host 0.0.0.0 --port 8000
+# Activate environment
+source venv/bin/activate
 
-pm2 save
-pm2 startup
+# Restart PM2 process
+/usr/bin/pm2 delete fastapi-app || true
+/usr/bin/pm2 start venv/bin/uvicorn app:app \
+  --name fastapi-app --interpreter bash -- --host 0.0.0.0 --port 8000
+
+/usr/bin/pm2 save
